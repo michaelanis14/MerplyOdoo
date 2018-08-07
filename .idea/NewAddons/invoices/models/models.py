15d81a4a32b4eb980776c854b2e828d96b088b51
osboxes <ragaamaherhann@gmail.com>
2018-08-07 11:44:42 -0400

    adding some views matching with AWB invoice

--- .idea/NewAddons/invoices/models/models.py
+++ .idea/NewAddons/invoices/models/models.py
@@ -2,13 +2,31 @@
 
 from odoo import models, fields
 
-class invoices(models.Model):
-     _name = 'account_invoicing'
+class master_Invoices(models.Model):
+     _name = 'account.invoice'
+     _inherit ='account.invoice'
 
+     package = fields.Integer('Package')
+     account = fields.Char('Account')
 
      Chipper = fields.Text()
-     Consignee = fields.Text()
-     Intermideate_consignee = fields.Text()
+     Consignee = fields.Selection(selection=[
+
+      ('agent1', 'Agent 1'),
+
+      ('agent2', 'Agent 2'), ],
+          string='Consignee')
+     Intermediate_consignee = fields.Selection(selection=[
+
+      ('agent1', 'Agent 1'),
+
+      ('agent2', 'Agent 2'), ],
+          string='Intermediate Consignee')
+
+     Exporting_carrier = fields.Char("Exporting Carrier")
+     Exporting_date = fields.Date("Exporting Date")
+
+
 
 #     @api.depends('value')
 #     def _value_pc(self):
