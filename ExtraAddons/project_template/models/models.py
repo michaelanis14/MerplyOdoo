b635dd9d8c1571279c8b21c2cdf4c86b80306462
osboxes <ragaamaherhann@gmail.com>
2018-09-01 11:41:15 -0400

    editing the module view

--- ExtraAddons/project_template/models/models.py
+++ ExtraAddons/project_template/models/models.py
@@ -5,69 +5,74 @@ from odoo import models,fields,api
 class project(models.Model):
     _name = 'project.main'
     _description = 'this model contains all the details of the bill'
+    _rec_name = 'consignee'
 
-    consignee = fields.Many2one('consignee.info', 'Consignee')
-    mawb = fields.Char('MAWB NO')
-    hawb = fields.Char('HAWB NO')
-    date = fields.Date('Invoice Date')
+# ========= adressess information ==================================================page2=============================================
+    shipper = fields.Many2one('res.partner', 'Shipper')
+    consignee = fields.Many2one('res.partner', 'Consignee')
+    agent_name = fields.Many2one('res.partner', 'Agent Name')
 
-    destination_airport = fields.Char('Airport of Destination')
-    departure_airport = fields.Char('Airport of Departure')
+    accounting_information =fields.Text('Accounting Information')
+    AITA_code =fields.Char('AITA Code')
+    account_no = fields.Char(string='Account No', relted='project.main.AITA_code', store=True)
 
-    cargo_connector = fields.One2many('cargo.info','cargo_project_connector', 'Cargo')
-    charges_connector = fields.One2many('charges.info','charges_project_connector', 'Charges')
-
-    totalCharges = fields.Float(string='Total', store=True, compute='_calCharges')
-
-    @api.one
-    @api.depends('charges_connector')
-    def _calCharges(self):
-        currentcharges = 0
-        for charge in self.charges_connector:
-            currentcharges = currentcharges + (charge.charge_value)
-
-            self.totalCharges = currentcharges
+    # ========================================Handling Information==============================================================================
 
 
-class consignee_info(models.Model):
-    _name = 'consignee.info'
-    _inherit = 'res.partner'
-    _description = 'contains Consignee Information'
+    handling_information = fields.Text('Handling Information')
+    _destination = fields.Char('Destination')
+    sci = fields.Char('SCI')
 
-    consignee_name = fields.Char('Consignee Name')
-    mypartner = fields.Many2one('res.partner')
-    consignee_street1 = fields.Char(related = 'mypartner.street', string='Address', store=True)
-    consignee_street2 = fields.Char(related='mypartner.street2', string='Street 2', store=True)
-    consignee_city = fields.Char(related='mypartner.city', string='City', store=True)
-    consignee_state = fields.Many2one(related='mypartner.state_id', string='State', store=True)
-    consignee_country = fields.Many2one(related='mypartner.country_id', string='Country', store=True)
+    # ==============================Cargo Information====================================================================================================
 
-
-    consignee_tel = fields.Char(related='mypartner.phone', string='Phone', store=True)
-    consignee_fax = fields.Char('Fax')
-    consignee_email = fields.Char(related='mypartner.email', string='Email', store=True)
-    consignee_salesperson = fields.Many2one(related='mypartner.user_id', string='Sales Person', store=True)
-
-
-class cargo_info(models.Model):
-    _name = 'cargo.info'
-    _description = 'this model contains the cargo iformation'
-
-    cargo_project_connector =fields.Many2one('project.main')
-    delivery_order = fields.Char('Delivery Order')
-    no_of_packages = fields.Integer('No Of Packages')
+    no_of_pieces = fields.Integer('No. of Pieces RCP')
     gross_weight = fields.Float('Gross Weight')
-    net_weight = fields.Float('Net Weight')
+    commodity_item_no = fields.Integer('Commodity Item No')
+    Chargeable_Wight = fields.Float('Chargeable Weight')
+    rate_charge = fields.Float('Rate Charge')
+    total = fields.Float('Total')
+
+    tax = fields.Float('Tax')
+
+    quantity = fields.Text('Nature and Quantity of Goods')
+
+    # ========= connectors ==================================================page2=============================================
+    routingO2M_connector = fields.One2many('routing.details','raoutingM2O_connector', 'Routing and distnation')
+    chargesO2M_connector = fields.One2many('charges.details','chargesM2O_connector', 'Charges Decleration')
 
 
-class charges_info(models.Model):
-    _name = 'charges.info'
-    _description = 'this model contains the cargo iformation'
+#============ Routing and distnation =======================================================page1==============================================
+class routing_details(models.Model):
+    _name = 'routing.details'
+    _description = 'this model contains Routing and distnation Information'
+    _rec_name = 'flight_no'
 
-    charges_project_connector =fields.Many2one('project.main')
-    charge_name = fields.Char('Charge Name')
-    charge_value = fields.Float('Charge Value')
-    totalCharges = fields.Float(string='Total', store= True, compute = '_calCharges')
+    raoutingM2O_connector = fields.Many2one('project.main')
+    airport_of_departure = fields.Char('Airport of Departure')
+    airport_of_destination = fields.Char('Airport of Destination')
+    to = fields.Char('TO')
+    by = fields.Char('BY')
+    flight_no = fields.Char('Flight No')
+    flight_date = fields.Date('Flight Date')
+
+#========= opertional shipping information ==================================================page2=============================================
+class charges_details(models.Model):
+    _name = 'charges.details'
+    _description = 'this model contains Charges Information'
+
+    chargesM2O_connector = fields.Many2one('project.main')
+    currency = fields.Many2many('res.currency', 'Currency')
+
+    charge_code = fields.Char('CHGS CODE')
+    ppd = fields.Boolean("PPD")
+    coll = fields.Boolean('COLL')
+
+    declared_carriage = fields.Char('Declared Value for Carriage')
+    declared_customs = fields.Char('Declared Value for Customs')
+
+    accouunt_insurance = fields.Char('Amount of Insurance')
+
+
 
 
 
