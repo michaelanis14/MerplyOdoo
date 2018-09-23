647ed87862241d4cf4c8fe62482df1b6f43f4871
osboxes <ragaamaherhann@gmail.com>
2018-09-23 11:38:45 -0400

    new view

--- awb_addons/air_waybill/models/Air_freight_bill_Model.py
+++ awb_addons/air_waybill/models/Air_freight_bill_Model.py
@@ -1,3 +1,4 @@
+import uuid
 
 from odoo import models,fields,api
 class Air_freight_bill(models.Model):
@@ -9,7 +10,7 @@ class Air_freight_bill(models.Model):
     mawb_afb_ID = fields.Many2one("mawb.model")
     hawb_afb_ID = fields.Many2one("hawb.model")
 
-    services_ID = fields.One2many('services.model', 'afb_sevices_IDs')
+    services_ID = fields.One2many('services.model', 'invoice_id')
 
 
     afb_no_of_pieces = fields.Integer(related='hawb_afb_ID.h_no_of_pieces',
@@ -29,9 +30,11 @@ class Air_freight_bill(models.Model):
 
     afb_weight = fields.Float(related='hawb_afb_ID.h_weight',
                        string='Weight', store=True, readonly=True)
-    price = fields.Float('Price', digits=(6, 2))
+    afb_price = fields.Float('Price', digits=(6, 2))
     mawb_a = fields.Many2one(related='hawb_afb_ID.mawb_IDs',
                              string='MAWB NO', store=True, readonly=True)
+    # ========= modifying existing fields ==============================================================================
+    partner_id = fields.Many2one(related='hawb_afb_ID.h_shipper')
 
 
 
@@ -41,5 +44,26 @@ class Air_freight_bill(models.Model):
     @api.depends('afb_weight','price')
     def _compute_frieght_charge(self):
 
-        return self.afb_weight * self.price
+        return self.afb_weight * self.afb_price
+
+
+
+    @api.one
+    @api.depends('services_ID.price_subtotal', 'tax_line_ids.amount', 'tax_line_ids.amount_rounding',
+                 'currency_id', 'company_id', 'date_invoice', 'type')
+    def _compute_amount(self):
+        round_curr = self.currency_id.round
+        self.amount_untaxed = sum(line.price_subtotal for line in self.services_ID)
+        self.amount_tax = sum(round_curr(line.amount_total) for line in self.tax_line_ids)
+        self.amount_total = self.amount_untaxed + self.amount_tax
+        amount_total_company_signed = self.amount_total
+        amount_untaxed_signed = self.amount_untaxed
+        if self.currency_id and self.company_id and self.currency_id != self.company_id.currency_id:
+            currency_id = self.currency_id.with_context(date=self.date_invoice)
+            amount_total_company_signed = currency_id.compute(self.amount_total, self.company_id.currency_id)
+            amount_untaxed_signed = currency_id.compute(self.amount_untaxed, self.company_id.currency_id)
+        sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.amount_total_company_signed = amount_total_company_signed * sign
+        self.amount_total_signed = self.amount_total * sign
+        self.amount_untaxed_signed = amount_untaxed_signed * sign
 
--- awb_addons/air_waybill/models/Services_Model.py
+++ awb_addons/air_waybill/models/Services_Model.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+import dp as dp
 
 from odoo import models,fields,api
 class Cargo_Model(models.Model):
     _name = 'services.model'
+    _inherit = "account.invoice.line"
     _rec_name = 'service_name'
     _description = 'Services Information'
 
-    afb_sevices_IDs = fields.Many2one('account.invoice')
+
+
+    #afb_sevices_IDs = fields.Many2one('account.invoice')
+    sevice_ID = fields.Many2one('services.model')
 
     service_name = fields.Selection([('f_ch', 'FOB Charges (EX-Works)'),
                                      ('fuel', 'FUEL'),
@@ -33,12 +38,31 @@ class Cargo_Model(models.Model):
                                      ('origin', 'CERTIFICATE OF ORIGIN'),
                                      ('collection', 'COLLECTION'),
                                      ],default='f_ch')
-
-    currency_ID = fields.Many2one('res.currency', 'Currency')
     sale = fields.Float('Sale')
     main = fields.Boolean('Main')
-    cost = fields.Float('Cost')
+    cost = fields.Monetary('Cost')
     description = fields.Text('Description')
     vendor =fields.Char('Vendor')
     lump_sum = fields.Boolean('Lumb Sum')
 
+    # overriding compute_price
+
+    @api.one
+    @api.depends('cost', 'discount', 'invoice_line_tax_ids',
+                 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
+                 'invoice_id.date_invoice', 'invoice_id.date')
+    def _compute_price(self):
+        currency = self.invoice_id and self.invoice_id.currency_id or None
+        price = self.cost * (1 - (self.discount or 0.0) / 100.0)
+        taxes = False
+        if self.invoice_line_tax_ids:
+            taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id,
+                                                          partner=self.invoice_id.partner_id)       #problem here
+        self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.cost
+        self.price_total = taxes['total_included'] if taxes else self.price_subtotal
+        if self.invoice_id.currency_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
+            price_subtotal_signed = self.invoice_id.currency_id.with_context(
+                date=self.invoice_id._get_currency_rate_date()).compute(price_subtotal_signed,
+                                                                        self.invoice_id.company_id.currency_id)
+        sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.price_subtotal_signed = price_subtotal_signed * sign
